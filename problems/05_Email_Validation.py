5ac943b8b0d03b47c1fe3cd26a624c3b9fe418ba
Sahil403-star <sahil.srivastava401@gmail.com>
2026-04-23 16:34:29 +0530

    Delete problems/05_Email_Validation.py

--- problems/05_Email_Validation.py
+++
@@ -1,8 +0,0 @@
-import re
-
-pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$"
-
-def validate_email(email):
-    return bool(re.match(pattern, email))
-
-print(validate_email("test@gmail.com"))
