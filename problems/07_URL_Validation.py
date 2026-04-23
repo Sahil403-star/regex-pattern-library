08111392536d00ed7af0c93515e8df885b9fd586
Sahil403-star <sahil.srivastava401@gmail.com>
2026-04-23 16:35:00 +0530

    Delete problems/07_URL_Validation.py

--- problems/07_URL_Validation.py
+++
@@ -1,8 +0,0 @@
-import re
-
-pattern = r"^(https?://)?(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}"
-
-def validate_url(url):
-    return bool(re.match(pattern, url))
-
-print(validate_url("https://google.com"))
